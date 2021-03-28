1640b481016d49afa374e84660db1db6c86f0a96
arzWZM <55745106+arzWZM@users.noreply.github.com>
2021-03-28 11:13:10 +0530

    Update CalculatorTest.java

--- src/test/java/my/simplejavaprojectwithjacoco/CalculatorTest.java
+++ src/test/java/my/simplejavaprojectwithjacoco/CalculatorTest.java
@@ -27,7 +27,7 @@ public class CalculatorTest {
 		assertEquals(3, result);
 		
 	}
-	
+/*	
 	@Test
 	public void testMultiply()
 	{
@@ -36,7 +36,7 @@ public class CalculatorTest {
 		int result = calculator.multiply(6, 2);
 		assertEquals(12, result);		
 	}
-
+*/
 	@Test
 	public void testDivide()
 	{
